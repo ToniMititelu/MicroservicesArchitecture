b40ea1f94c177d31a7f670c4d50d0a70e9886efc
antonio.mititelu <antonio.mititelu@tremend.com>
2021-05-11 18:52:26 +0300

    add orders microservice configuration

---
+++ server/orders-microservice/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const MONGO_PORT = 27017;
+
+const mongoUri = `mongodb://${process.env.db_service_name}:${MONGO_PORT}/${process.env.db_name}`;
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true 
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+    res.json({'message': 'ok hot reload'})
+});
+
+app.listen(PORT, () => console.log(`All set up. Listening on ${PORT}!`))
