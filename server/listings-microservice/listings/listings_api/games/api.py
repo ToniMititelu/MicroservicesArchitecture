2eb4a76b01bd0106bf232d0cee64c1e9898c7771
antonio.mititelu <antonio.mititelu@tremend.com>
2021-05-14 20:52:33 +0300

    add mine endpoint to get only game listings that a user owns

--- server/listings-microservice/listings/listings_api/games/api.py
+++ server/listings-microservice/listings/listings_api/games/api.py
@@ -38,8 +38,15 @@ def create_game_listing(request, payload: GameListingIn):
     return 201, game_listing
 
 @router.get("/", response=List[GameListingOut], auth=None)
-def get_game_listings(request):
+def get_game_listings(request, user_id: str = None):
     game_listings = GameListing.objects.all()
+    if user_id:
+        return game_listings.filter(user_id=user_id)
+    return game_listings
+
+@router.get("/mine/", response=List[GameListingOut])
+def get_my_game_listings(request):
+    game_listings = GameListing.objects.filter(user_id=request.auth['id'])
     return game_listings
 
 @router.get("/{game_listing_id}/", response=GameListingOut, auth=None)
@@ -52,10 +59,33 @@ def update_game_listing(request, game_listing_id: int, payload: GameListingIn):
     game_listing = get_object_or_404(GameListing, id=game_listing_id)
     if not is_admin(request.auth) and not is_owner(request.auth, game_listing):
         return 403, {"message": "Only admins and owners can edit listings"}
-    for attr, value in payload.dict().items():
+    
+    payload = payload.dict()
+    category_id = payload.pop('category_id', None)
+    try:
+        game_category = GameCategory.objects.get(id=category_id)
+    except GameCategory.DoesNotExist:
+        return 400, {"message": "Game category not found"}
+    
+    currency_id = payload.pop('currency_id', None)
+    try:
+        currency = Currency.objects.get(id=currency_id)
+    except Currency.DoesNotExist:
+        return 400, {"message": "Currency not found"}
+    
+    platform_id = payload.pop('platform_id', None)
+    try:
+        platform = Platform.objects.get(id=platform_id)
+    except Platform.DoesNotExist:
+        return 400, {"message": "Platform not found"}
+
+    payload['category'] = game_category
+    payload['currency'] = currency
+    payload['platform'] = platform
+    for attr, value in payload.items():
         setattr(game_listing, attr, value)
     game_listing.save()
-    return game_listing
+    return 200, game_listing
 
 @router.delete("/{game_listing_id}/", response={200: Success, 403: Error})
 def delete_game_listing(request, game_listing_id: int):
