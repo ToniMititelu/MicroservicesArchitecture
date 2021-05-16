4c4a1bcf4ccdbf4a63668ec14dd8f5dc8a1792d6
antonio.mititelu <antonio.mititelu@tremend.com>
2021-05-16 18:48:52 +0300

    add models for shippment service

---
+++ server/shipments-microservice/models/Address.js
@@ -0,0 +1,37 @@
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+
+const AddressSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    street: {
+        type: String,
+        required: true
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+    other: {
+        type: String,
+        required: false
+    },
+    isDefault: {
+        type: Boolean,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+moongose.model('addresses', AddressSchema);
---
+++ server/shipments-microservice/models/Shipment.js
@@ -0,0 +1,17 @@
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+
+const ShipmentSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    addressId: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+moongose.model('shipments', ShipmentSchema);
