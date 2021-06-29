881ad3106ed5012d7f2c7c9bbf63413e9cb5e845
antonio.mititelu <antonio.mititelu@tremend.com>
2021-06-29 21:32:41 +0300

    add navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MenubarModule } from 'primeng/menubar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestAuthComponent } from './components/test-auth/test-auth.component';
 import { TokenInterceptor } from './interceptors/token.interceptor';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestAuthComponent
+    TestAuthComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MenubarModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
---
+++ client/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  items: MenuItem[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
