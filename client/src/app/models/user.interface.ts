a3b40b8fc2a37d08f33250b6ad128cd26cf8306a
antonio.mititelu <antonio.mititelu@tremend.com>
2021-06-29 17:05:58 +0300

    user interface

---
+++ client/src/app/models/user.interface.ts
@@ -0,0 +1,18 @@
+export interface UserRegister {
+  username: string;
+  email: string;
+  password_1: string;
+  password_2: string;
+}
+
+export interface UserLogIn {
+  username: string;
+  password: string;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  email: string;
+  role: string;
+}
