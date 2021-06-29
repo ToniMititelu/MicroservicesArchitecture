ddbc25ca519a0ff6350afe12bd2210c39c3bb297
antonio.mititelu <antonio.mititelu@tremend.com>
2021-06-29 22:23:16 +0300

    update navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,25 +1,37 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
+import { PrimeTemplate } from 'primeng/api';
+import { TabViewModule } from 'primeng/tabview';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestAuthComponent } from './components/test-auth/test-auth.component';
 import { TokenInterceptor } from './interceptors/token.interceptor';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestAuthComponent,
-    NavbarComponent
+    NavbarComponent,
+    LogInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MenubarModule,
+    InputTextModule,
+    TabViewModule,
+    ButtonModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { AuthService } from 'src/app/services/auth.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,11 +10,52 @@ import { MenuItem } from 'primeng/api';
 })
 export class NavbarComponent implements OnInit {
 
-  items: MenuItem[] = [];
+  showLogIn = false;
+  showRegister = false;
 
-  constructor() { }
+  items: MenuItem[] = [
+    {
+      label: 'Admin',
+      icon: 'pi pi-fw pi-id-card',
+      routerLink: 'home'
+    },
+    {
+      label: 'Profile',
+      icon: 'pi pi-fw pi-user',
+    },
+    {
+      label: 'Log In',
+      icon: 'pi pi-fw pi-sign-in',
+      command: this.logIn,
+    },
+    {
+      label: 'Register',
+      icon: 'pi pi-fw pi-user-plus',
+      command: this.register,
+    },
+    {
+      label: 'Log Out',
+      icon: 'pi pi-fw pi-sign-out',
+      command: this.logOut,
+    }
+  ];
+
+  constructor(readonly authService: AuthService,
+              readonly localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
+  }
+
+  logIn() {
+    this.showLogIn = true;
+  }
+
+  register() {
+    this.showRegister = true;
+  }
+
+  logOut() {
+    this.localStorageService.clearLocalStorage();
   }
 
 }
