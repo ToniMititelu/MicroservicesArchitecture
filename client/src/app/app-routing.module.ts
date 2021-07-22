0936ccaef959991566fbd7fae24b838ec2dca1a0
antonio.mititelu <antonio.mititelu@tremend.com>
2021-07-22 18:30:04 +0300

    add routing to login and register components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {CreateUpdateListingsComponent} from './components/create-update-listings/create-update-listings.component';
 import {ListingsComponent} from './components/listings/listings.component';
 import {MainComponent} from './components/main/main.component';
 import {ListingDetailsComponent} from './components/listing-details/listing-details.component';
+import {LogInComponent} from './components/log-in/log-in.component';
+import {RegisterComponent} from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {
     path: 'home',
     component: MainComponent
+  },
+  {
+    path: 'log-in',
+    component: LogInComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   },
   {
     path: 'listings',
@@ -37,4 +47,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
