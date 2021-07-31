4efd32ba5ad67b94363b3a7e37d32621b9da4d32
antonio.mititelu <antonio.mititelu@tremend.com>
2021-07-31 20:08:04 +0300

    disable toke expiration

--- server/authentication-microservice/controllers/controller.js
+++ server/authentication-microservice/controllers/controller.js
@@ -110,7 +110,7 @@ const generateTokens = async (user) => {
         role: user.role
     }
 
-    const access_token = jwt.sign({ data: data }, jwt_secret, {expiresIn: 5});
+    const access_token = jwt.sign({ data: data }, jwt_secret);
     const refresh_token = jwt.sign({ data: {id: data.id}}, jwt_secret);
 
     return {
