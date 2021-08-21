dff09a2b31ef60b66dbf735b9196da3ae140e570
antonio.mititelu <antonio.mititelu@tremend.com>
2021-08-21 18:48:53 +0300

    add method to create chat room in chat service

--- client/src/app/services/chat.service.ts
+++ client/src/app/services/chat.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { DocumentClass } from '../models/document.model';
 import { Socket } from 'ngx-socket-io';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  baseUrl = 'http://localhost:8080/api/chat';
+
   currentDocument = this.socket.fromEvent<DocumentClass>('document');
   documents = this.socket.fromEvent<string[]>('documents');
 
-  constructor(private socket: Socket) {
+  constructor(private socket: Socket,
+              private http: HttpClient) {
   }
 
-  getChatRoom(): void {
-    return;
+  createChatRoom(body: any): Observable<any> {
+    const url = `${this.baseUrl}/rooms/`;
+    return this.http.post(url, body);
   }
 
   getDocument(id: string): void {
